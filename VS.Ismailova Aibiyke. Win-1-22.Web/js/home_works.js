418bef710e38529708fa4fc5e914af32d98a3435
Aibiyke77 <116260407+Aibiyke77@users.noreply.github.com>
2023-12-22 00:23:03 +0600

    Add files via upload

---
+++ VS.Ismailova Aibiyke. Win-1-22.Web/js/home_works.js
@@ -0,0 +1,87 @@
+// gmail input
+
+const gmailInput = document.querySelector('#gmail_input')
+const gmailCheck = document.querySelector('#gmail_button')
+const gmailResult = document.querySelector('#gmail_result')
+
+const regExp = /^(?![!@#$%^&*()+=\-[\]{}|;:'",.<>/?]).*[0-9a-zA-Z.]{6,30}@gmail.com$/
+
+
+gmailCheck.onclick = () =>{
+    if (regExp.test(gmailInput.value)) {
+        gmailResult.innerHTML = 'С…РѕРєР°РіРµ РѕРґРѕР±СЂРёР»!!!'
+        gmailResult.style.color = 'green'
+    }else {
+        gmailResult.innerHTML = 'С…РѕРєР°РіРµ РЅРµ РѕРґРѕР±СЂСЏРµС‚ РІРІРµРґС‘РЅРЅС‹Р№ gmail'
+        gmailResult.style.color = 'red'
+    }
+}
+
+//recurs
+
+const childBlock = document.querySelector('.child_block')
+
+let positionX = 0
+let positionY = 0
+
+const moveBlock =()=>{
+    if (positionX < 650 && positionY === 0){
+        positionX++
+        childBlock.style.left=`${positionX}px`
+        setTimeout(moveBlock, 1)
+    }else if (positionX >= 650 && positionY < 400){
+        positionY++
+        childBlock.style.top=`${positionY}px`
+        setTimeout(moveBlock, 1)
+    }else if (positionX > 0  && positionY === 400){
+        positionX--
+        childBlock.style.left=`${positionX}px`
+        setTimeout(moveBlock, 1)
+    }else if (positionX === 0 && positionY > 0){
+        positionY--
+        childBlock.style.top=`${positionY}px`
+        setTimeout(moveBlock, 1)
+    }
+
+
+}
+moveBlock()
+
+//time
+const seconds = document.querySelector('#seconds')
+const start_button = document.querySelector('#start')
+const stop_button = document.querySelector('#stop')
+const resume_button = document.querySelector('#resume')
+const reset_button = document.querySelector('#reset')
+
+let time = 0
+let interval
+const timer =()=>{
+    if(time < 720){
+        time++
+        seconds.innerHTML = time
+    }
+}
+
+start_button.addEventListener('click', () => {
+    interval = setInterval(timer, 10)
+
+})
+
+stop_button.addEventListener('click', ()=>{
+    clearInterval(interval)
+    start_button.disabled = true
+})
+
+resume_button.addEventListener('click', ()=>{
+    interval = setInterval(timer, 10)
+
+})
+
+reset_button.addEventListener('click', ()=>{
+    clearInterval(interval);
+    time = 0;
+    seconds.innerHTML = time
+    start_button.disabled = false
+
+})
---
+++ VS.Ismailova Aibiyke. Win-1-22.Web/js/main.js
@@ -0,0 +1,119 @@
+// RANDOM COLOR GENERATOR
+
+const buttonsColor = document.querySelectorAll('.btn-color')
+const javaScript = document.querySelector('#js-color')
+
+const generateRandomColor = () => {
+    const hexCodes = '0123456789ABCDEF'
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    return '#' + color
+}
+
+const setRandomColors = () => {
+    buttonsColor.forEach((buttonColor) => {
+        buttonColor.innerHTML = generateRandomColor()
+        buttonColor.onclick = (event) => javaScript.style.color = event.target.innerHTML
+    })
+}
+
+window.onload = () => setRandomColors()
+window.onkeydown = (event) => {
+    if (event.code.toLowerCase() === 'space') {
+        event.preventDefault()
+        setRandomColors()
+    }
+}
+
+// SLIDER BLOCK
+
+const slides = document.querySelectorAll('.slide')
+const next = document.querySelector('#next')
+const prev = document.querySelector('#prev')
+let index = 0
+
+const hideSlide = () => {
+    slides.forEach((slide) => {
+        slide.style.opacity = 0
+        slide.classList.remove('active_slide')
+    })
+}
+const showSlide = (i = 0) => {
+    slides[i].style.opacity = 1
+    slides[i].classList.add('active_slide')
+}
+
+hideSlide()
+showSlide(index)
+
+
+const autoSlider = (i = 0) => {
+    setInterval(() => {
+        i++
+        if (i > slides.length - 1) {
+            i = 0
+        }
+        hideSlide()
+        showSlide(i)
+    }, 10000)
+}
+
+next.onclick = () => {
+    index < slides.length - 1 ? index++ : index = 0
+    hideSlide()
+    showSlide(index)
+}
+
+prev.onclick = () => {
+    index > 0 ? index-- : index = slides.length - 1
+    hideSlide()
+    showSlide(index)
+}
+
+autoSlider(index)
+
+//json-photo-card
+
+const button = document.querySelector('.btn_push');
+const hokagesInfo = document.querySelector('.JS');
+let infoDisplayed = false;
+
+button.addEventListener('click', async () => {
+    if (infoDisplayed) {
+        hokagesInfo.innerHTML = '';
+        infoDisplayed = false;
+        return;
+    }
+
+    try {
+        const response = await fetch('data/peoples.json');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+    const hokages = await response.json();
+
+    hokages.forEach(person => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+                <img src="${person.image}" alt="${person.name}">
+                <h5>${person.name}</h5>
+                <h6>Number: ${person.number}</h6>
+                <span>Abilities: ${person.abilities}</span>
+            `;
+        hokagesInfo.append(div);
+
+        div.addEventListener('mouseenter', () => {
+            div.style.backgroundColor = 'orange';
+        })
+        div.addEventListener('mouseleave', () => {
+            div.style.backgroundColor = null;
+        })
+    })
+        infoDisplayed = true;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+})
